369a0767f152fb519b140fc590ed5a34be153d56
Calli Bonnet <calli@paradox.ai>
2019-09-14 21:23:27 -0700

    added more questions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -3,21 +3,51 @@
 //these are all of my questions and correct answers 
 const quizQuestions = [
     {
-        question: "What is my full name", 
-        chocies: ["Sally Blue", "Calli Bonnet", "Jonny Chase", "Betty White"],
-        correctAnswer: "Calli Bonnet"
+        question: "What Disney Princess lived with 7 other people? ", 
+        chocies: ["Sally Blue", "Calli Bonnet", "Snow White", "Betty White"],
+        correctAnswer: "Snow White"
     }, 
 
     {
-        question: "What is your favorite sport?", 
-        chocies: ["Soccer", "Baseball", "Football", "Swimming"],
-        correctAnswer: "Soccer"
+        question: "What Disney Princess lost a glass slipper?", 
+        chocies: ["Soccer", "Snow White", "Football", "Cinderella"],
+        correctAnswer: "Cinderella"
     }, 
 
     {
-        question: "What town did you live at?", 
-        chocies: ["Denver", "Lions", "Broomfield", "Scottsdale"],
-        correctAnswer: "Lions"
+        question: "What Disney Princess lived under the sea?", 
+        chocies: ["Ariel", "Cinderella", "Snow White", "Scottsdale"],
+        correctAnswer: "Ariel"
+    },
+
+    {
+        question: "What Disney Princess has a pet tiger?", 
+        chocies: ["Jasmine", "Ariel", "Cinderella", "Scottsdale"],
+        correctAnswer: "Jasmine"
+    },
+
+    {
+        question: "Who kissed a frog?", 
+        chocies: ["Aurora", "Belle", "Jasmine", "Tiana"],
+        correctAnswer: "Tiana"
+    },
+
+    {
+        question: "Which Disney Princess loved to explore the sa?", 
+        chocies: ["Belle", "Jasmine", "Moana", "Mulan"],
+        correctAnswer: "Moana"
+    },
+    
+    {
+        question: "Which Disney Princess fell in love with a Beast?", 
+        chocies: ["Ariel", "Merida", "Belle", "Mulan"],
+        correctAnswer: "Belle"
+    },
+
+    {
+        question: "Which Disney Princess was locked in a tower?", 
+        chocies: ["Moana", "Pocahontas", "Rapunzel", "Tiana"],
+        correctAnswer: "Rapunzel"
     }
 
 ];
